3e00c100507bc8edc81826e295e49c60a85e0f30
Miguel <migue7490@gmail.com>
2017-03-18 22:00:16 -0300

    cola.hs modified

--- Pruebas/cola.hs
+++ Pruebas/cola.hs
@@ -3,7 +3,6 @@ data Cola a = Cola [a] deriving (Show)
 encolar :: a -> Cola a -> Cola a
 encolar x (Cola xs) = Cola (xs ++ [x])
 
-
 desencolar :: Cola a -> (a, Cola a)
 desencolar (Cola []) = error "Cola vacía"
 desencolar (Cola xs) = (head xs, Cola $ tail xs)
